b0d8d4bfffeaa3fac8fee7d217817c4366d1dfcb
CARLOS VIERA <pisis_13_3@hotmail.com>
2022-01-19 19:51:52 -0500

    Cambios en App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.repetirCadena=this.repetirCadena.bind(this)
+    this.state={
+      cadena:""
+    }
+  }
+  repetirCadena(event){
+    this.setState({
+      cadena:event.target.value
+    })
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" value={this.state.cadena} onChange={this.repetirCadena} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.cadena}</p>
       </div>
-    );
+    )
   }
 }
 
